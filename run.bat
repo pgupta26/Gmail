b9e2b3d5ba9e7e6778ffbb1a6ec759ae0ad63c5b
pgupta26 <parshant.gupta00@gmail.com>
2019-08-22 15:02:39 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,3 +8,36 @@ e5543937/Y0ursM0tivational
 e3021171/k@7MBc&8)J1dTi#29
 
 e3013249/W&x3@6XqM(e12Z?f
+
+
+Equifax Credit Files - 30 
+
+1. Rebuild data button is missing.
+2. Total volumn clolumn is appearing at the end. It should come 
+after volumn processed. (As per my understanding this should not 
+be an issue because columns are draggable.)
+3. Data validation not required as per DBA.
+
+FCBT Billing Report - 35 - Still same issue exists.
+
+First Effort General PGM Postage - 36
+
+1. Underscore is appearing in the name of the column header.
+2. ADF Mail Date and Postage Cost and Cost Per Piece columns
+value not appearing.
+3. Data validation not required as per DBA.
+
+First Effort Inquiry Postage - 38
+
+1. Underscore is appearing in the name of the column header.
+2. ADF Mail Date and Postage Cost and Cost Per Piece columns
+value not appearing.
+3. Data validation not required as per DBA.
+
+Prudentail Weekly Usage - 95
+1. Data validation not required as per DBA.
+
+
+
+
+
